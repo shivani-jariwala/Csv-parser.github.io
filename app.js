141bc77bd978458bd8294cf0fd9aa21a35e48f68
Shivani Jariwala <jariwalashivani1105@gmail.com>
2022-12-16 18:11:51 +0530

    frontend allowed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const productRoutes = require('./routes/routes.js');
 const cors = require('cors');
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:'https://frontend-one.onrender.com', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
